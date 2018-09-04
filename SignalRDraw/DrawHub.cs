8bc76ebd08a870b770b48fb4a1d4c2fa3077a864
sergey.ermolaev <sergey.ermolaev@base4web.ru>
2018-09-04 18:33:59 +0300

    Drawing into chat

---
+++ SignalRDraw/DrawHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRDraw.Models;
+
+namespace SignalRDraw
+{
+    public class DrawHub : Hub
+    {
+        public void Send (Data data)
+        {
+            Clients.AllExcept(Context.ConnectionId).addLine(data);
+        }
+    }
+}
---
+++ SignalRDraw/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SignalRDraw.Models
+{
+    public class Data
+    {
+        [JsonProperty("startX")]
+        public float StartX { get; set; }
+        [JsonProperty("startY")]
+        public float StartY { get; set; }
+        [JsonProperty("endX")]
+        public float EndX { get; set; }
+        [JsonProperty("endY")]
+        public float EndY { get; set; }
+    }
+}
